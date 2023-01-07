f93c43c79d9c940ac2a0a8341c36849127719c7a
K. M. Zawad Monsur <zawadmonsur1@gmail.com>
2023-01-08 00:39:12 +0600

    skeleton for the project

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
+import colors from './src/constants/Colors'
+import tempData from './tempData'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={styles.divider} />
+                    <Text style={styles.title}>
+                        Todo <Text style={{ fontWeight: '300', color: colors.blue }}>List</Text>
+                    </Text>
+                    <View style={styles.divider} />
+                </View>
+
+                <View style={{ marginVertical: 48 }}>
+                    <TouchableOpacity style={styles.addlist}>
+                        <AntDesign name='plus' size={16} color={colors.blue}></AntDesign>
+                    </TouchableOpacity>
+
+                    <Text style={styles.add}>Add List</Text>
+                </View>
+
+                <View style={{ height: 275, paddingLeft: 32 }}>
+                    <FlatList
+                        data={tempData}
+                        keyExtractor={item => item.name}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        renderItem={({ item }) => (
+                            <View>
+                                <Text>{item.name} </Text>
+                            </View>
+                        )}
+                    />
+                </View>
+
+                <View></View>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    divider: {
+        backgroundColor: colors.lightblue,
+        height: 1,
+        flex: 1,
+        alignSelf: 'center'
+    },
+    title: {
+        fontSize: 38,
+        fontWeight: '800',
+        color: colors.black,
+        paddingHorizontal: 64
+    },
+    addlist: {
+        borderWidth: 2,
+        borderColor: colors.lightblue,
+        borderRadius: 4,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    add: {
+        color: colors.blue,
+        fontWeight: '600',
+        fontSize: 14,
+        marginTop: 8
+    }
+})
---
+++ tempData.js
@@ -0,0 +1,33 @@
+export default tempData = [
+    {
+        name: 'Plan a trip',
+        color: '#24a6d9',
+        todos: [
+            {
+                title: 'Book a flight',
+                completed: false
+            },
+
+            {
+                title: 'Check passport',
+                completed: true
+            },
+
+            {
+                title: 'Hotel check',
+                completed: false
+            }
+        ]
+    },
+
+    {
+        name: 'Errands',
+        color: '#8022d9',
+        todos: [
+            {
+                title: 'buy egg',
+                completed: false
+            }
+        ]
+    }
+]
